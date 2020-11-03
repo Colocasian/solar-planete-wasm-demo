5e4f0eca38158668c672d7a8ae16d8fdf2b08b89
Rishvic Pushpakaran <rishvic@gmail.com>
2020-11-03 11:05:09 +0530

    responsive orbital radii scaling

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import drawPlanets from "./src/drawPlanets";
 
 import("planete-wasm")
   .then(({ PlanetSystem }) => {
-    const scale_down = 8e8;
+    const scale_down = 5e11;
 
     const canvas = document.getElementById("planet-canvas");
 
@@ -21,17 +21,17 @@ import("planete-wasm")
 
     const planet_system = PlanetSystem.new();
 
-    planet_system.add_planet(5.972e24, 1.52e11, 0, 0, 2.929e4); // Earth
     planet_system.add_planet(1.989e30, 0, 0, 0, 0); // Sun
+    planet_system.add_planet(5.972e24, 1.52e11, 0, 0, 2.929e4); // Earth
     planet_system.add_planet(4.867e24, 1.08939e11, 0, 0, 3.478e4); // Venus
     planet_system.add_planet(3.285e23, 6.98169e10, 0, 0, 3.886e4); // Mercury
     planet_system.add_planet(6.39e23, 2.492e11, 0, 0, 2.2e4); // Mars
     planet_system.add_planet(2.2e14, -8.766e10, 0, 0, 5.458e4); // Halley's comet
 
-    const radii = [5, 15, 4.75, 1.915, 2.66, 1];
+    const radii = [15, 5, 4.75, 1.915, 2.66, 1];
     const colors = [
-      "#0077be",
       "#fdb813",
+      "#0077be",
       "#bbb7ab",
       "#e2e2e2",
       "#a1251b",
--- src/drawPlanets.js
+++ src/drawPlanets.js
@@ -3,6 +3,8 @@ import { parse } from "papaparse";
 export default function drawPlanets(planet_system, visuals, canvas) {
   canvas.ctx.fillStyle = "#000";
   canvas.ctx.fillRect(0, 0, canvas.width, canvas.height);
+  const local_scale_down =
+    visuals.scale_down / Math.min(canvas.width, canvas.height);
 
   parse(planet_system.output(), {
     header: false,
@@ -12,8 +14,8 @@ export default function drawPlanets(planet_system, visuals, canvas) {
     canvas.ctx.fillStyle = visuals.colors[idx];
     canvas.ctx.beginPath();
     canvas.ctx.arc(
-      parseFloat(planet[0]) / visuals.scale_down + canvas.width / 2,
-      parseFloat(planet[1]) / visuals.scale_down + canvas.height / 2,
+      parseFloat(planet[0]) / local_scale_down + canvas.width / 2,
+      parseFloat(planet[1]) / local_scale_down + canvas.height / 2,
       visuals.radii[idx],
       0,
       2 * Math.PI
